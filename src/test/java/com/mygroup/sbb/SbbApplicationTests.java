ed7d14472c1718be43c5df5c7e705641a3788bd0
taegung <sungminyun12@daum.net>
2023-07-31 16:02:08 +0900

    writer

--- src/test/java/com/mygroup/sbb/SbbApplicationTests.java
+++ src/test/java/com/mygroup/sbb/SbbApplicationTests.java
@@ -14,6 +14,10 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-    
+        for (int i = 1; i <= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content,null);
+        }
     }
 }
